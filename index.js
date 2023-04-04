3d2cbc9ef635037943a896f7474d90037b7a4249
Daniel Dalaota <daniel40799@gmail.com>
2023-04-05 01:23:25 +0800

    new card edits

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
+let cards = []
+
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
-function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+
+
+let player = {
+    name: "Dan",
+    chips: 100
+}
+
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
+
+function startGame(){
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame();
+}
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for(let i =0; i<cards.length; i++){
+        cardsEl.textContent+= cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -24,3 +47,30 @@ function startGame() {
 }
 
 // 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+
+
+function newCard(){
+    if(isAlive===true && hasBlackJack===false){
+    console.log("Drawing a new card from the deck")
+    let card = getRandomCard();
+    sum+=card;
+    cards.push(card)
+    console.log(cards)
+    renderGame();
+    }
+}
+
+
+
+function getRandomCard(){
+    let cardNumber = Math.floor(Math.random()*13)+1
+    if(cardNumber===1){
+        return 11
+    }
+    else if(cardNumber>10){
+        return 10
+    }
+    else{
+        return cardNumber
+    }
+}
